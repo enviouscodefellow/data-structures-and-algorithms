62dd9c012b24bcaef6b8bfbf4bd081c04e37019e
Nick <enviouscodefellow@outlook.com>
2023-01-10 14:30:38 -0700

    challenge 10 complete

--- python/data_structures/stack.py
+++ python/data_structures/stack.py
@@ -1,12 +1,37 @@
+import pytest
+from data_structures.invalid_operation_error import InvalidOperationError
+
+class Node:
+    def __init__(self, value=None, next=None):
+        self.value = value
+        self.next = next
+
+
 class Stack:
     """
     Put docstring here
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.top = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def push(self, value):
+        self.top = Node(value, self.top)
+
+    def pop(self):
+        if not self.top:
+            raise InvalidOperationError("Cannot pop from an empty stack.")
+        value = self.top.value
+        self.top = self.top.next
+        return value
+
+    def peek(self):
+        if not self.top:
+            raise InvalidOperationError("Cannot peek at an empty stack.")
+        return self.top.value
+
+    def is_empty(self):
+        return self.top is None
+
+class InvalidOperationError(Exception):
+    pass
