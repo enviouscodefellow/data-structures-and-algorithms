ab86f9d8337fdbca8f13e5e3268b4728a8075453
Nick <enviouscodefellow@outlook.com>
2023-01-10 20:18:48 -0700

    challenge 12 complete

--- python/code_challenges/stack_queue_animal_shelter.py
+++ python/code_challenges/stack_queue_animal_shelter.py
@@ -1,13 +1,31 @@
+import pytest
 from data_structures.queue import Queue
 
 
 class AnimalShelter:
-    pass
+    def __init__(self):
+        self.dog_queue = Queue()
+        self.cat_queue = Queue()
 
+    def enqueue(self, animal):
+        if isinstance(animal, Dog):
+            self.dog_queue.enqueue(animal)
+        elif isinstance(animal, Cat):
+            self.cat_queue.enqueue(animal)
 
-class Dog:
-    pass
+    def dequeue(self, pref):
+        if pref == "dog":
+            return self.dog_queue.dequeue()
+        elif pref == "cat":
+            return self.cat_queue.dequeue()
+        return None
 
 
 class Cat:
-    pass
+    def __str__(self):
+        return 'cat'
+
+
+class Dog:
+    def __str__(self):
+        return 'dog'
